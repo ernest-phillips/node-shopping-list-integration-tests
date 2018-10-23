169a4841eb8202fba5f051430653628321c18e3e
ernest-phillips <ernestlphillips@gmail.com>
2018-10-22 21:23:56 -0700

    fixed it

--- test/test-recipes.js
+++ test/test-recipes.js
@@ -1,138 +1,106 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 
-const { app, runServer, closeServer } = require("../server");
+const {app, runServer, closeServer} = require('../server');
 
-// this lets us use *expect* style syntax in our tests
-// so we can do things like `expect(1 + 1).to.equal(2);`
-// http://chaijs.com/api/bdd/
-const expect = chai.expect;
+const should = chai.should();
 
-// This let's us make HTTP requests
-// in our tests.
-// see: https://github.com/chaijs/chai-http
 chai.use(chaiHttp);
 
-describe("Recipes", function() {
-  // Before our tests run, we activate the server. Our `runServer`
-  // function returns a promise, and we return the that promise by
-  // doing `return runServer`. If we didn't return a promise here,
-  // there's a possibility of a race condition where our tests start
-  // running before our server has started.
+
+describe('Recipes', function() {
+
   before(function() {
     return runServer();
   });
 
-  // although we only have one test module at the moment, we'll
-  // close our server at the end of these tests. Otherwise,
-  // if we add another test module that also has a `before` block
-  // that starts our server, it will cause an error because the
-  // server would still be running from the previous tests.
   after(function() {
     return closeServer();
   });
 
-  // test strategy:
-  //   1. make request to `/shopping-list`
-  //   2. inspect response object and prove has right code and have
-  //   right keys in response object.
-  it("should list items on GET", function() {
-    // for Mocha tests, when we're dealing with asynchronous operations,
-    // we must either return a Promise object or else call a `done` callback
-    // at the end of the test. The `chai.request(server).get...` call is asynchronous
-    // and returns a Promise, so we just return it.
-    return chai
-      .request(app)
-      .get("/recipes")
+  it('should list recipes on GET', function() {
+    // recall that we manually add some recipes to `Recipes`
+    // inside `recipesRouter.js`. Later in this course,
+    // once we're using a database layer, we'll seed
+    // our database with test data, and we can form our expectations
+    // about what GET should return, based on what we know about
+    // the state of our database.
+    return chai.request(app)
+      .get('/recipes')
       .then(function(res) {
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a("array");
 
-        // because we create three items on app load
-        expect(res.body.length).to.be.at.least(1);
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+
+        res.body.should.have.length.of.at.least(1);
+
         // each item should be an object with key/value pairs
-
-        // for `name` and `ingredients`.
-        const expectedKeys = ["name", "ingredients"];
-
-       
+        // for `id`, `name` and `ingredients`.
         res.body.forEach(function(item) {
-          expect(item).to.be.a("object");
-          expect(item).to.include.keys(expectedKeys);
+          item.should.be.a('object');
+          item.should.include.keys('id', 'name', 'ingredients');
         });
       });
   });
 
-  // test strategy:
-  //  1. make a POST request with data for a new item
-  //  2. inspect response object and prove it has right
-  //  status code and that the returned object has an `id`
-  it("should add an item on POST", function() {
-
-    const newItem = { name: "chocolate cake", ingredients:['cocoa','eggs','butter'] };
+  it('should add a recipe on POST', function() {
+    const newRecipe = {
+        name: 'coffee', ingredients: ['ground coffee', 'hot water']};
     return chai.request(app)
-      .post("/recipes")   
-      .send(newItem)
+      .post('/recipes')
+      .send(newRecipe)
       .then(function(res) {
         res.should.have.status(201);
         res.should.be.json;
-        res.should.be.a('object');
-        res.body.to.include.keys("id","name", "ingredients");
-    res.body.name.should.equal(newItem.name);
-    res.body.ingredients.should.be.a('array');
-//what is going on here        res.body.ingredients.should.include.members(newItem.ingredients);
-        
+        res.body.should.be.a('object');
+        res.body.should.include.keys('id', 'name', 'ingredients');
+        res.body.name.should.equal(newRecipe.name);
+        res.body.ingredients.should.be.a('array');
+        res.body.ingredients.should.include.members(newRecipe.ingredients);
       });
   });
 
+  it('should update recipes on PUT', function() {
 
-  it("should update items on PUT", function() {
-    
     const updateData = {
-      name: "foo",
-      ingredients: ["bizz","bang"]     
+      name: 'foo',
+      ingredients: ['bizz', 'bang']
     };
 
-    return (chai.request(app)
-        // first have to get so we have an idea of object to update
-        .get("/recipes")
-        .then(function(res) {
-          updateData.id = res.body[0].id;
-          // this will return a promise whose value will be the response
-          // object, which we can inspect in the next `then` block. Note
-          // that we could have used a nested callback here instead of
-          // returning a promise and chaining with `then`, but we find
-          // this approach cleaner and easier to read and reason about.
-          return chai.request(app)
-            .put(`/recipes/${updateData.id}`)
-            .send(updateData);
-        })
-        // prove that the PUT request has right status code
-        // and returns updated item
-        .then(function(res) {
-          res.should.have.status(204)
-        });
-    
+    return chai.request(app)
+      // first have to get recipes so have `id` for one we
+      // want to update. Note that once we're working with databases later
+      // in this course get the `id` of an existing instance from the database,
+      // which will allow us to isolate the PUT logic under test from our
+      // GET interface.
+      .get('/recipes')
+      .then(function(res) {
+        updateData.id = res.body[0].id;
+
+        return chai.request(app)
+          .put(`/recipes/${updateData.id}`)
+          .send(updateData)
+      })
+      .then(function(res) {
+        res.should.have.status(204);
+      });
   });
 
-  // test strategy:
-  //  1. GET recipe items so we can get ID of one
-  //  to delete.
-  //  2. DELETE an item and ensure we get back a status 204
-  it("should delete items on DELETE", function() {
-    return (chai.request(app)
-        // first have to get so we have an `id` of item
-        // to delete
-        .get("/recipes")
-        .then(function(res) {
-
-          return chai.request(app)
-          .delete(`/recipes/${res.body[0].id}`);
-
-        })
-        .then(function(res) {
-          expect(res).to.have.status(204);
-        });    
+  it('should delete recipes on DELETE', function() {
+    return chai.request(app)
+      // first have to get recipes so have `id` for one we want
+      // to delete. Note that once we're working with databases later
+      // in this course, we'll be able get the `id` of an existing instance
+      // directly from the database, which will allow us to isolate the DELETE
+      // logic under test from our GET interface
+      .get('/recipes')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/recipes/${res.body[0].id}`)
+      })
+      .then(function(res) {
+        res.should.have.status(204);
+      });
   });
 });
